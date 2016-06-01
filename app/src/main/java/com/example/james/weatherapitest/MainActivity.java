046605a165ff7133c53c944c5d88c861f78fc16e
James Orrell <jamesthefourth@gmail.com>
2016-05-31 22:19:06 -0700

    Updated to accept simple, unchecked user input for city name and display formatted results

--- app/src/main/java/com/example/james/weatherapitest/MainActivity.java
+++ app/src/main/java/com/example/james/weatherapitest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +23,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     TextView responseView;
+    EditText currentInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,32 +35,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void searchButton(View v){
+
+        currentInput = (EditText)findViewById(R.id.enterText);
+        String currentCity = currentInput.getText().toString();
         responseView = (TextView)findViewById(R.id.responseView);
-        networkRequest one = new networkRequest(responseView);
+        //pass in the city name to the network request -- plan to get this later from the user location
+        networkRequest one = new networkRequest(responseView, currentCity);
         responseView.setText(one.toString());
     }
 }
 
 //initiate new thread for network connected api call in order to avoid slowing UI thread.
  class networkRequest extends AsyncTask<String,String,String> {
-
+    String cityName;
     private Exception exception;
      TextView responseView;
      HttpURLConnection urlConnection;
 
-    protected networkRequest(TextView textView){
+    protected networkRequest(TextView textView, String city){
         super.execute();
         responseView = textView;
+        cityName = city;
 
     }
 
 
+
      protected String doInBackground(String... args) {
+
 
          StringBuilder result = new StringBuilder();
          JSONObject resultObject = null;
          try {
-             URL url = new URL("http://api.wunderground.com/api/f1650fb7e0ae610e/conditions/q/CA/San_Francisco.json");
+             URL url = new URL("http://api.wunderground.com/api/f1650fb7e0ae610e/conditions/q/CA/"+ cityName +".json");
              urlConnection = (HttpURLConnection) url.openConnection();
              InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
@@ -80,10 +89,15 @@ public class MainActivity extends AppCompatActivity {
 
          try {
                 JSONObject values = resultObject.getJSONObject("current_observation");
-             return values.getString("temperature_string");
+                StringBuilder output = new StringBuilder();
+                output.append("The current temperature in ");
+                output.append(cityName);
+                output.append(" is ");
+                output.append(values.getString("temperature_string"));
+             return output.toString();
 
          }catch (JSONException e){
-             return "bad data, JSONException";
+             return "Sorry, we don't have info for that city yet! Please try again.";
          }
      }
 
