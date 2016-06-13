015d9994241ce5c17d27dc15db6fc3c2e33761a3
James Orrell <jamesthefourth@gmail.com>
2016-06-12 17:47:22 -0700

    fixed bugs with color, rounded differenceTemp, added a click me to find out if warming button that is invisible until the search is complete, still handling async tasks in a sloppy way but it should work. added nicer formatting of response on the differenceView.

--- app/src/main/java/com/example/james/weatherapitest/MainActivity.java
+++ app/src/main/java/com/example/james/weatherapitest/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,14 +39,23 @@ public class MainActivity extends AppCompatActivity {
     double averageTemp;
     double differenceTemp;
     String completeCityName;
+    networkRequest one;
+    secondNetworkRequest two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        differenceViewTemp = (Button)findViewById(R.id.differenceViewTemp);
+        differenceViewTemp.setVisibility(View.INVISIBLE);
     }
 
     public void searchButton(View v){
+
+        //reset values
+        currentTemp = 0;
+        averageTemp = 0;
+        differenceTemp = 0;
 
 
 
@@ -60,10 +70,8 @@ public class MainActivity extends AppCompatActivity {
         responseView = (TextView)findViewById(R.id.responseView);
         responseViewAlmanac = (TextView)findViewById(R.id.responseViewAlmanac);
         differenceViewTemp = (TextView)findViewById(R.id.differenceViewTemp);
-        
-        //clear current difference view values
-        differenceViewTemp.setText("");
-        differenceViewTemp.setBackgroundColor(Color.LTGRAY);
+
+        differenceViewTemp.setVisibility(View.INVISIBLE);
 
         cityViewRequest = (TextView)findViewById(R.id.cityViewRequest);
 
@@ -75,45 +83,56 @@ public class MainActivity extends AppCompatActivity {
         //get historic temperature from city
         secondNetworkRequest two = new secondNetworkRequest(responseViewAlmanac, currentCity);
 
-
-
     }
 
-    //method to check if an async task has completed
+    //new button to click once data has loaded
+    public void amIWarmingButton(View v){
+
+        setDifferenceViewTemp();
+    }
+
+    //method to check if an async task has completed ---- currently not in use
     private boolean asyncStatusCheck(AsyncTask task){
 
         if(task.getStatus() == AsyncTask.Status.PENDING){
-            try {
-                wait(100);
-                asyncStatusCheck(task);
-            }catch(InterruptedException e){
+
                 return false;
-            }
+
         }
 
         if(task.getStatus() == AsyncTask.Status.RUNNING){
-            try {
-                wait(100);
-                asyncStatusCheck(task);
-            }catch(InterruptedException e){
+
                 return false;
-            }
+
         }
 
         if(task.getStatus() == AsyncTask.Status.FINISHED){
+
             return true;
         }
         return false;
     }
-
+    //method to calculate the difference between current and almanac data and set color of diff view
     private void setDifferenceViewTemp(){
 
+            differenceTemp = currentTemp - averageTemp;
 
-        differenceTemp = currentTemp - averageTemp;
-        differenceViewTemp.setText(String.valueOf(differenceTemp));
-        differenceViewTemp.setBackgroundColor(Color.parseColor(getColor(differenceTemp)));
+            String differenceTempString = String.format("%.2f", differenceTemp);
+            differenceViewTemp.setText(differenceTempString + " degrees " + isWarmer(differenceTemp) + " today.");
+            differenceViewTemp.setBackgroundColor(Color.parseColor(getColor(differenceTemp)));
+
     }
 
+    //method to return warmer/cooler than average string depending on the difference in temp
+    private String isWarmer(double d){
+        if(d >= 0)
+        {
+            return getString(R.string.warmer);
+        }
+        else{
+            return getString(R.string.cooler);
+        }
+    }
 
 
     /**
@@ -128,9 +147,9 @@ public class MainActivity extends AppCompatActivity {
         if (temp >= 7.0) {
             color = "#FF0000";
         } else if (temp < 5.0 && temp >= 2.0) {
-            color = "FFA500";
+            color = "#FFA500";
         } else if (temp < 2.0 && temp >= 0.5) {
-            color = "FFFF00";
+            color = "#FFFF00";
         } else if (temp < 0.5 && temp >= -0.5) {
             color = "#008000";
         } else if (temp < -0.5 && temp >= -2.0) {
@@ -141,10 +160,99 @@ public class MainActivity extends AppCompatActivity {
         return color;
     }
 
+/**
+ * The following three embedded classes are network requests designed to perform three functions:
+ * 1 - use weather underground autocomplete API to get a valid city name from user input
+ * 2 - get current weather conditions for this city
+ * 3 - get historic data for this city
+ */
+
+
+    /**
+     * Class that processes the input string and used the wunderground API to
+     * return the best match to the search
+     */
+    class cityRequest extends AsyncTask<String,String,String> {
+        String cityName;
+        private Exception exception;
+        TextView cityViewRequest;
+        HttpURLConnection urlConnection;
+
+        protected cityRequest(TextView textView,String city){
+
+            cityViewRequest = textView;
+            cityName = city;
+
+            super.execute();
+
+        }
+
+
+        protected String doInBackground(String... args) {
+
+
+            StringBuilder result = new StringBuilder();
+            JSONObject resultObject = null;
+            try {
+
+                //THIS IS WHERE WE USE AUTO COMPLETE TO GET THE PROPER CITY NAME
+
+                URL url = new URL("http://autocomplete.wunderground.com/aq?query=" + cityName);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+                resultObject = new JSONObject(result.toString());
+
+            }catch( Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+
+
+            try {
+                //THIS IS WHERE WE VALIDATE THE USER INPUT OF CITYNAME
+                //build the output string from the JSON data and the user input city name
+
+                //precaution/testing if/else to make sure we get some kind of city name returned
+                if(resultObject != null) {
+
+                    JSONArray values = resultObject.getJSONArray("RESULTS");
+                    StringBuilder output = new StringBuilder();
+
+                    output.append(values.getJSONObject(0).get("name"));
+                    completeCityName = (values.getJSONObject(0).get("name").toString());
+                    return output.toString();
+                }
+                else
+                    return cityName;
+
+            }catch (JSONException e){
+                return "City name not recognized";
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            cityViewRequest.setText(response);
+
+
+
+        }
+
+    }
+
 
     //initiate new thread for network connected api call in order to avoid slowing UI thread.
 //this class handles gathering the current temperature, the next class will handle almanac data
-//*** planning to refactor this code so we don't have huge duplicate methods here...****
+
     class networkRequest extends AsyncTask<String,String,String> {
         String cityName;
         private Exception exception;
@@ -200,6 +308,12 @@ public class MainActivity extends AppCompatActivity {
             }catch (JSONException e){
                 return "Sorry, we don't have info for that city yet! Please try again.";
             }
+        }
+
+        protected void onProgressUpdate(){
+            TextView responseView = (TextView) findViewById(R.id.responseView);
+            responseView.setText("fetching current condtions...");
+
         }
 
         protected void onPostExecute(String response) {
@@ -274,91 +388,13 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(response);
 
             responseViewAlmanac.setText(response);
-
-            setDifferenceViewTemp();
-
-        }
-
-    }
-
-    /**
-     * Class that processes the input string and used the wunderground API to
-     * return the best match to the search
-     */
-    class cityRequest extends AsyncTask<String,String,String> {
-        String cityName;
-        private Exception exception;
-        TextView cityViewRequest;
-        HttpURLConnection urlConnection;
-
-        protected cityRequest(TextView textView,String city){
-
-            cityViewRequest = textView;
-            cityName = city;
-
-            super.execute();
-
-        }
-
-
-        protected String doInBackground(String... args) {
-
-
-            StringBuilder result = new StringBuilder();
-            JSONObject resultObject = null;
-            try {
-
-                //THIS IS WHERE WE USE AUTO COMPLETE TO GET THE PROPER CITY NAME
-
-                URL url = new URL("http://autocomplete.wunderground.com/aq?query=" + cityName);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-                resultObject = new JSONObject(result.toString());
-
-            }catch( Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                urlConnection.disconnect();
-            }
-
-
-            try {
-                //THIS IS WHERE WE VALIDATE THE USER INPUT OF CITYNAME
-                //build the output string from the JSON data and the user input city name
-
-                //precaution/testing if/else to make sure we get some kind of city name returned
-                if(resultObject != null) {
-
-                    JSONArray values = resultObject.getJSONArray("RESULTS");
-                    StringBuilder output = new StringBuilder();
-
-                    output.append(values.getJSONObject(0).get("name"));
-                    completeCityName = (values.getJSONObject(0).get("name").toString());
-                    return output.toString();
-                }
-                else
-                    return cityName;
-
-            }catch (JSONException e){
-                return "Sorry, we don't have info for that city yet! Please try again.";
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            super.onPostExecute(response);
-            cityViewRequest.setText(response);
-
-
+            differenceViewTemp.setVisibility(View.VISIBLE);
+            differenceViewTemp.setBackgroundColor(Color.LTGRAY);
+            differenceViewTemp.setText(R.string.warmingButton);
 
         }
 
     }
+
+
 }
